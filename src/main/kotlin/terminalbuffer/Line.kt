f4d5eaf74be9f229f6d00fd2602cc5ab8ddf9ae8
vljn <121915880+vljn@users.noreply.github.com>
2026-03-16 05:04:34 +0100

    feat: add Line class for managing a row of cells

---
+++ src/main/kotlin/terminalbuffer/Line.kt
@@ -0,0 +1,19 @@
+package io.github.vljn.terminalbuffer
+
+class Line(width: Int) {
+    val cells: Array<Cell> = Array(width) { Cell() }
+
+    fun clear() {
+        for (i in cells.indices) {
+            cells[i] = Cell()
+        }
+    }
+
+    operator fun get(index: Int): Cell {
+        return cells[index]
+    }
+
+    operator fun set(index: Int, cell: Cell) {
+        cells[index] = cell
+    }
+}
